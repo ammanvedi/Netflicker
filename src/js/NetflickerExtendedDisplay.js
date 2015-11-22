1b76fe668b068b7ef5b94c31ca5324399945838c
amman vedi <amman.vedi@pulselive.com>
2015-11-22 22:22:36 +0000

    create extended display initial interface

---
+++ src/js/NetflickerExtendedDisplay.js
@@ -0,0 +1,21 @@
+/**
+ * Created by ammanvedi on 22/11/2015.
+ */
+
+
+Netflicker.ExtendedDisplay = function() {
+
+}
+
+Netflicker.ExtendedDisplay.prototype.showDisplay = function() {
+
+}
+
+Netflicker.ExtendedDisplay.prototype.hideDisplay = function() {
+
+}
+
+Netflicker.ExtendedDisplay.prototype.setListeners = function() {
+
+}
+
